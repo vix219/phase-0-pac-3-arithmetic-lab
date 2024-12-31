4775730fdf0743d3edbc0b4e25dbc948ed3f82a1
VictoriaCarvalho <vix219@icloud.com>
2024-12-31 13:06:21 -0600

    adds lab

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+    return "something";  }
+
+  function subtract() {
+    return "something";
+  }
+
+  function multiply() {
+    return "something";
+  }
+
+  function divide() {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b
+  }
+
+  function multiply(a, b) {
+    return a * b
+  }
+
+  function divide(a, b) {
+    return a / b
+  }
+  
+  function increment(n) {
+    return ++n
+  }
+
+  function decrement(n) {
+    return --n
+  }
+
+  function makeInt(n) {
+   return parseInt(n, 10)
+  }
+
+  function preserveDecimal(n) {
+    return parseFloat(n)
+   }
